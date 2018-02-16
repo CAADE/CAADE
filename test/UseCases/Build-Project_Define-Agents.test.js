5d1bc4477a55eaa0f209336cab4c77f1902f602d
madajaju <darren@pulsipher.org>
2018-02-15 16:45:13 -0800

    Addition of the DevOps Actor.

--- test/UseCases/Build-Project_Define-Agents.test.js
+++
@@ -1,9 +0,0 @@
-var Promise = require('bluebird');
-
-describe('Use Case Build-Project', function () {
-  describe('Define-Agents Scenario', function () {
-    it('Define-Agents Primary Test', function (done) {
-       done();
-    });
-  });
-});
--- test/UseCases/Build-Project_Define-Stages.test.js
+++
@@ -1,9 +0,0 @@
-var Promise = require('bluebird');
-
-describe('Use Case Build-Project', function () {
-  describe('Define-Stages Scenario', function () {
-    it('Define-Stages Primary Test', function (done) {
-       done();
-    });
-  });
-});
